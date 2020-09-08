b2596ccc9bc4167f41bef3c57ae0c27c25c2ddf2
Ramkumar | Front-end Dev <64207937+Ramkumar-45@users.noreply.github.com>
2020-09-08 14:41:01 +0530

    Update settings.py

--- Blogwebsite/settings.py
+++ Blogwebsite/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ramkumarblog15@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ramkumar@45'
+EMAIL_HOST_USER = 'mail id'
+EMAIL_HOST_PASSWORD = 'mail id password'
